495b711f76a810cc0d0381e2410373c3069f40af
Crowdbotics <team@crowdbotics.com>
2020-07-10 04:07:44 +0000

    add package blueprint-react-native-tutorial-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Tutorial76817Navigator from '../features/Tutorial76817/navigator';
 import NotificationList76789Navigator from '../features/NotificationList76789/navigator';
 import Settings76788Navigator from '../features/Settings76788/navigator';
 import Settings76780Navigator from '../features/Settings76780/navigator';
@@ -19,6 +20,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Tutorial76817: { screen: Tutorial76817Navigator },
 NotificationList76789: { screen: NotificationList76789Navigator },
 Settings76788: { screen: Settings76788Navigator },
 Settings76780: { screen: Settings76780Navigator },
